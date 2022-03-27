39f99e2909d033afd3944faf711c44ca0bebe3a8
svyatjes <cvity6692@gmail.com>
2022-03-28 01:02:15 +0300

    docker done

---
+++ t1/t1_1/hello.py
@@ -0,0 +1,5 @@
+import time
+
+while True:
+    print('hello im first container')
+    time.sleep(2)
---
+++ t1/t1_2/python.py
@@ -0,0 +1,6 @@
+import time
+
+while True:
+    print('hello, i\'m Third container')
+    time.sleep(2)
+
